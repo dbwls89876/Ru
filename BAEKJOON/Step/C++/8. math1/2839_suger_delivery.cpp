78e170cd972c69305ed05ef9c42681c2c500e1a8
Ru <dbwls89876@naver.com>
2020-11-20 09:13:02 +0900

    11/20

---
+++ BAEKJOON/Step/C++/8. math1/2839_suger_delivery.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+//문제틀림 
+int main(){
+	while(1)
+	int* pArr = (int *)malloc(sizeof(int) * 4);
+
+
+	
+	/*int n;
+	int e5, e3;
+	cin>>n;
+	
+
+	
+	if(n>1)
+		n=-1;
+	else{
+		n=e5+e3;
+	}
+	
+	cout<<n;*/
+}
