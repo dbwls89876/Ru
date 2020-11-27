cdb9c05a6d681c14e7e2bdd9c7590a1b0b2a86d0
Ru <dbwls89876@naver.com>
2020-11-27 09:59:39 +0900

    11/27

--- BAEKJOON/Step/C++/8. math1/2869_snail.cpp
+++ BAEKJOON/Step/C++/8. math1/2869_snail.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
-//a-b
+
 int main(){
-	long a, b, v, i=0;
+	long a, b, v;
 	cin>>a;
 	cin>>b;
 	cin>>v;
-	
-	cout<<(v-a)/(a-b)+1;
+	if((v-a)%(a-b)==0)
+	cout<<((v-a)/(a-b))+1;
+	else
+	cout<<((v-a)/(a-b))+2;
 	return 0;
 }
