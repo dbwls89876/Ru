de005be6fc5fd736a27b1c23bf4e6e5856a215e3
Ru <dbwls89876@naver.com>
2020-12-03 11:29:40 +0900

    12/03

--- BAEKJOON/Step/C++/8. math1/2775_chair_man.cpp
+++ BAEKJOON/Step/C++/8. math1/2775_chair_man.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main(){
@@ -6,5 +7,26 @@ int main(){
 	int k, n; // Ãþ, È£ 
 	int arr[15];
 	int temp[15];
+	cin>>t;
+
+	
+	for(int l = 0; l<t; l++){
+		cin>>k;
+		cin>>n;
+		for(int i = 0; i<n; i++){
+			arr[i] = i+1;
+		}		
+		if(k>0){
+			for(int i = 0; i<k; i++){
+				for(int j = 1; j<n; j++){
+					copy(arr, arr+15, temp);
+					arr[j]=temp[j=1]+temp[j];
+					cout<<arr[j];
+				}
+			}
+		}
+		cout<<arr[n-1]<<"\n";
+	}
+	
 		
 }
