5059261e0c76d79e61dec4d48e5800600c613037
Ru <dbwls89876@naver.com>
2021-05-06 22:21:28 +0900

    21/05/06_math2

--- BAEKJOON/Step/11. Bruteforce/7568_build.cpp
+++ BAEKJOON/Step/11. Bruteforce/7568_build.cpp
@@ -5,7 +5,7 @@ class Build{
 	int t;
 	public:
 	Build(){}
-	Build(int test){
+	void init(int test){
 		t = test;
 		for(int i = 0; i<t; i++){
 			scanf("%d %d", &arr[i][0], &arr[i][1]);
@@ -13,6 +13,8 @@ class Build{
 			arr[i][2]++;
 		}
 	}
+	
+	//랭크 분류 
 	void build(){	
 		for(int i = 0; i<t; i++){
 			for(int j = i+1; j<t; j++){
@@ -29,15 +31,20 @@ class Build{
 		}	
 	}
 	
+	//랭크 출력 
 	void rankPrint(){
 		for(int i = 0; i<t; i++)
 		printf("%d ", arr[i][2]);
 	}
 	
+	//유닛테스트 
 	void test(){		
+		//테스트1 **************************
+		printf("Test1\n");
 		int tf = 1;
-		int n = 70, m = 180;
+		int n = 99, m = 197;
 		int answer[100] = {5, 4, 3, 2, 1};
+		t = 5;
 		for(int i = 0; i<5; i++){
 			arr[i][0] = n+i;
 			arr[i][1] = m+i;
@@ -47,29 +54,80 @@ class Build{
 		build();
 
 		std::list<int> list;
-		for(int i = 0; i<answer[i]!=0; i++){
-			if(arr[i][2] != answer[i]){
-				printf("arr=%d, ans = %d", arr[i][2], answer[i]);
-				tf=0;
-				break;
-			}
-				
-		}
-		printf("%d\n", tf);
+		trueORfalse(answer, &tf);
 		list.push_back(tf);
 		
+		//테스트2 ************************************ 
+		printf("Test2\n");
+		for(int i = 0; i<5; i++){
+			answer[i] = 1;
+		}
+		t = 5;
+		for(int i = 0; i<5; i++){
+			arr[i][0] = n;
+			arr[i][1] = m;
+			arr[i][2] = 1;
+		}
+		
+		build();
+		trueORfalse(answer, &tf);
+		list.push_back(tf);
+		
+		//테스트3 ************************************ 
+		printf("Test3\n");
+		for(int i = 1; i<=5; i++){
+			answer[i] = i;
+		}
+		t = 5;
+		for(int i = 0; i<5; i++){
+			arr[i][0] = n-i;
+			arr[i][1] = m-i;
+			arr[i][2] = 1;
+		}
+		
+		build();
+		trueORfalse(answer, &tf);
+		list.push_back(tf);
+		
+		//테스트4 ************************************ 
+		printf("Test4\n");
+		for(int i = 0; i<5; i++){
+			answer[i] = 1;
+		}
+		answer[0] = 2;
+		t = 5;
+		for(int i = 0; i<5; i++){
+			arr[i][0] = n+i;
+			arr[i][1] = m+i;
+			arr[i][2] = 1;
+		}
+		
+		arr[2][0] == 99;
+		arr[4][0] == 99;
+		
+		build();
+		trueORfalse(answer, &tf);
+		list.push_back(tf);
+		
+		//테스트 케이스 성공/실패 출력 
 		std::list<int>::iterator iter; 
- 
 	    for(iter=list.begin(); iter!=list.end(); iter++) 
 	    { 
 	        if(*iter)
-	        	printf("true");
+	        	printf("true\n");
 	        else
-	        	printf("false");
+	        	printf("false\n");
 	    } 
-
 	}
 	
+	void trueORfalse(int answer[], int * tf){
+		for(int i = 0; answer[i]!=0; i++){
+			if(arr[i][2] != answer[i]){
+				*tf=0;
+				break;
+			}		
+		}
+	}
 };
 
 
