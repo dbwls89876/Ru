880cbe95a60a2eec58b8fd0f48645f8ca276b421
Ru <dbwls89876@naver.com>
2021-05-09 23:09:40 +0900

    21/05/09_bruteforce

---
+++ BAEKJOON/Step/11. Bruteforce/1018_chess.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <algorithm>
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int arr[n][m];
+	int min = 1000;
+	for(int l = 0; l<=n-8; l++)
+	for(int k = 0; k<=m-8; k++)
+	for(int i = 0; i<8; i++){
+		for(int j = 0; j<8; j++){
+			if()
+		}
+	}
+}
