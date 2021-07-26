83ae404a5a827e4a86cc8d42cd012b8afe56db93
Ru <dbwls89876@naver.com>
2021-07-26 23:52:10 +0900

    sort pos doing...

---
+++ BAEKJOON/Step/12. sort/11650_sort_pos.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+//	int n;
+//	int x, y;
+//	vector <vector<int> > arr(n, vector<int>(2,0));
+//	scanf("%d", &n);
+//	for(int i = 0; i<n; i++){
+//		scanf("%d %d", &x, &y);
+////		printf("%d %d\n", x, y);
+//		arr[i][0] = x;
+//		arr[i][1] = y;
+//	}
+//	
+////	sort(arr.begin(), arr.end());
+//	printf("%d", n);
+//	for(int i = 0; i<n; i++){
+//		
+//		printf("%d %d\n", arr[i][0], arr[i][1]);
+//		
+//	}
+//		
+//		
+	
+	 
+}
