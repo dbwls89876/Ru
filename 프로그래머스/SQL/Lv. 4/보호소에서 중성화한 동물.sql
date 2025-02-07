8c31846c9851305bce1222b11dd71aa4144abbdc
Ru <57591391+dbwls89876@users.noreply.github.com>
2025-02-07 22:44:00 +0900

    Create 보호소에서 중성화한 동물.sql
    재활훈련 막바지

---
+++ 프로그래머스/SQL/Lv. 4/보호소에서 중성화한 동물.sql
@@ -0,0 +1,10 @@
+-- ANIMAL_INS 보호소테이블 / ANIMAL_OUTS 입양보낸 테이블
+-- 중성화 : SEX_UPON_INTAKE, SEX_UPON_OUTCOME
+-- 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회
+
+SELECT INS.ANIMAL_ID, INS.ANIMAL_TYPE, INS.NAME FROM ANIMAL_INS AS INS
+    INNER JOIN ANIMAL_OUTS AS OUTS ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE 1=1
+    AND SEX_UPON_INTAKE LIKE '%Intact%'
+    AND SEX_UPON_OUTCOME REGEXP 'Neutered|Spayed'
+ORDER BY INS.ANIMAL_ID
